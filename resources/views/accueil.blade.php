0579a6a7ac02125c008b9e3d890815608235994f
Diallo Mamadou <mamadoudialloyorgo@gmail.com>
2022-08-03 18:50:13 +0000

    Modiffication de la page d'accueil

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hello-city</title>
+
+    </head>
+    <body>
+        <h1>Bonjour dèpuis Conakry</h1>
+        <p>Il est actuellement {{date("h : i A")}}.</p>
+
+        <footer>
+            <p>&copy; copyright {{date("Y")}} &middot; <a href="#">Apropos-nous</a></p>
+        </footer>
+    </body>
+</html>
